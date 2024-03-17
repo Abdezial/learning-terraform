105e772c8a62132856ba415eb91ff077f22aa603
Abdezial <34236805+Abdezial@users.noreply.github.com>
2024-03-18 04:02:04 +0530

    remove alb target

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "blog_autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arn    = moudle.blog_alb.target_group_arns
+  target_group_arns   = moudle.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id            = data.aws_ami.app_ami.id
@@ -56,15 +56,6 @@ module "blog_alb" {
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
 
   http_tcp_listeners = [
     {
