f60ce318a3f6034618dd217aeb1b38b83512e1c1
Abdezial <34236805+Abdezial@users.noreply.github.com>
2024-03-18 04:41:08 +0530

    Error Fix - {} in name prefix

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "$(var.env.name)-"
+      name_prefix      = "${var.env.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
